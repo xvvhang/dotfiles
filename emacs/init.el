7cce75543daac18ced534229a982f28340577527
xuhang <xvvhang@gmail.com>
2025-07-28 17:59:13 +0800

    [emacs] add denote

--- emacs/init.el
+++ emacs/init.el
@@ -70,16 +70,13 @@
              (("C-s" . consult-line)
               ("C-x b" . consult-buffer)
               ("M-y" . consult-yank-pop)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package markdown-mode
+             :ensure t)
+(use-package denote
+             :ensure t
+             :config
+             (setq denote-directory (expand-file-name"~/Documents/Notes/"))
+             (setq denote-prompts '(title keywords signature))
+             (setq denote-file-type 'markdown-yaml)
+             (setq denote-known-keywords nil)
+             (setq denote-date-identifier-format ""))
