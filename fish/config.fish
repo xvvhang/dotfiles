2f130e0055cceca84d02a454c9f35fef9d07711b
xuhang <xvvhang@gmail.com>
2025-08-15 12:03:14 +0800

    [fish] add set-theme function

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,37 @@ set -gx TLRC_CONFIG "$XDG_CONFIG_HOME/tldr/config.toml"
 set -gx _ZO_DATA_DIR "$XDG_DATA_HOME/zoxide"
 set -gx PNPM_HOME "$XDG_DATA_HOME/pnpm"
 
+function set-theme
+  set themes gruvbox tokyonight
+  set selected_theme (printf "%s\n" $themes | fzf --prompt="Select a theme: ")
+
+  if test -z "$selected_theme"
+    echo "No theme selected. Exiting."
+    exit 1
+  end
+
+  set ghostty_dir $XDG_CONFIG_HOME/ghostty
+  set ghostty_theme_file $ghostty_dir/$selected_theme
+  set ghostty_current_file $ghostty_dir/current
+  if test -f $ghostty_theme_file
+    cp $ghostty_theme_file $ghostty_current_file
+    echo "Ghostty set to $selected_theme."
+  else
+    echo "Theme file '$theme_file' not found for Ghostty."
+  end
+
+  # Change nvim theme
+  set nvim_theme_dir $XDG_CONFIG_HOME/nvim/lua/themes
+  set nvim_theme_file $nvim_theme_dir/$selected_theme.lua
+  set nvim_current_file $nvim_theme_dir/current.lua
+  if test -f $nvim_theme_file
+    cp $nvim_theme_file $nvim_current_file
+    echo "Neovim set to $selected_theme."
+  else
+    echo "Theme file '$nvim_theme_file' not found for Neovim."
+  end
+end
+
 abbr bis 'brew install'
 abbr bus 'brew uninstall'
 abbr bup 'brew update'
@@ -52,6 +83,8 @@ abbr ghce 'gh copilot explain'
 
 alias n='nvim'
 alias g='lazygit'
+
+alias st='set-theme'
 
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$XDG_CONFIG_HOME'
 alias lazydot='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$XDG_CONFIG_HOME'
