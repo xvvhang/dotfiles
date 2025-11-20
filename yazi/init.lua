1f40606457c4e992b54ac814888f77c136ed0365
xuhang <xvvhang@gmail.com>
2025-11-20 10:21:42 +0800

    [yazi] add full-border plugin and change lines styles

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,39 +1,42 @@
+require("full-border"):setup()
 require("yatline"):setup({
-  show_background = false,
+  -- separators
   section_separator = { open = "", close = "" },
-	part_separator = { open = "", close = "" },
-	inverse_separator = { open = "", close = "" },
+  part_separator = { open = "", close = "" },
+  inverse_separator = { open = "", close = "" },
 
+  -- sections
+  style_a = {
+    fg = "black",
+    bg_mode = {
+      normal = "gray",
+      select = "lightyellow",
+      un_set = "lightred"
+    }
+  },
+
+  -- tabs
+  tab_width = 0,
+
+  -- background
+  show_background = false,
+
+  -- lines
+  display_status_line = false,
   header_line = {
-    left = {
-      section_a = {
+		left = {
+			section_a = {},
+			section_b = {},
+			section_c = {
         {type = "line", custom = false, name = "tabs", params = {"left"}},
-      },
-      section_b = {},
-      section_c = {}
-    },
+      }
+		},
     right = {
-      section_a = {},
-      section_b = {},
-      section_c = {}
-    }
-  },
-  status_line = {
-    left = {
-      section_a = {},
-      section_b = {
-        {type = "string", custom = false, name = "hovered_path"},
+			section_a = {
+        {type = "string", custom = false, name = "cursor_position"},
       },
-      section_c = {}
-    },
-    right = {
-      section_a = {},
-      section_b = {
-        {type = "string", custom = false, name = "hovered_size"},
-        {type = "coloreds", custom = false, name = "permissions"},
-        {type = "coloreds", custom = false, name = "count"},
-      },
-      section_c = {}
+			section_b = {},
+			section_c = {}
     }
-  },
+  }
 })
