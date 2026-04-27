c5b131a2d9fe072babc39387f09fca63f87a2ac2
xuhang <xvvhang@icloud.com>
2026-04-27 17:11:48 +0800

    theme: use kanagawa wave

--- .config/nvim/lua/themes/kanagawa.lua
+++ .config/nvim/lua/themes/kanagawa.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd('colorscheme kanagawa-dragon')
+    vim.cmd('colorscheme kanagawa-wave')
     vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "bg" })
   end
 }
